e0193a30df14ce82b9e11e45afa181efb4725639
leonel <leonel@DESKTOP-0SDEIO0>
2024-07-27 15:57:22 -0300

    chore: add flag to enable nextjs experimental feature only when testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env.local" });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    testProxy: true,
+    testProxy: process.env.APP_ENV === "automated_tests",
   },
 };
 
